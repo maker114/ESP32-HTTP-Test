c5c346b6fb61ca62921516889fda694b3ffddbb0
Maker114 <2739305114@qq.com>
2024-09-10 17:19:14 +0800

    更改了程序结构，完全修复了按键有关的问题，加入了有关HTTP获取失败重连的有关内容

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,16 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <ESP32Time.h>
-// #include <OneWire.h>
 #include <DallasTemperature.h>
 #define ONE_WIRE_BUS 32
 
 // 定义子函数
 void weather_update(void);
 void Button_CallBack(void);
+void Display_Mode1(void);
+void Display_Mode2(void);
+void Button_Scan(void);
+void HTTP_LinkError_Handle(void);
 
 // 定义WiFi密码及SSID
 const char *password = "00000000";
@@ -39,20 +42,20 @@ int nowtime;          // 北京时间
 ESP32Time rtc(3600 * 8); // offset in seconds GMT+8
 
 // 初始化变量
-int key_num = 0;    // 按键值
-float PotValue = 0; // 电压值
+int key_num = 0;            // 按键值
+float PotValue = 0;         // 电压值
+uint16_t Time_count = 0;    // 时间计数
+String response, response2; // HTTP响应
 
 // 初始化标志位
 int Update_Flag = 0; // 更新标志位，为了避免在同一时间内反复触发
-int key_flag = 0;    // 按键标志位，为了避免在按键按下后被视为连按
+int Button_Flag = 0; // 按键标志位，为了避免在按键按下后被视为连按
 int mode_flag = 0;   // 模式标志位，为了在多个模式中进行切换
-
 void setup()
 {
   // 初始化IO
-  pinMode(17, INPUT_PULLUP);                     // 控制按键
-  pinMode(2, OUTPUT);                            // 控制LED
-  attachInterrupt(17, Button_CallBack, FALLING); // 配置按键中断
+  pinMode(17, INPUT_PULLUP); // 控制按键
+  pinMode(2, OUTPUT);        // 控制LED
 
   // 初始化OLED
   u8g2.begin();
@@ -60,8 +63,8 @@ void setup()
   u8g2.setFont(u8g2_font_wqy12_t_gb2312); // 设置字体
 
   // 初始化串口
-  Serial.begin(9600);
-  Serial.println("Hello World!\r\n");
+  // Serial.begin(9600);
+  // Serial.println("Hello World!\r\n");
 
   // 显示连接UI
   u8g2.setCursor(0, 12);
@@ -94,19 +97,22 @@ void setup()
   HTTPClient http;                                    // 创建HTTP对象
   http.begin(url + "?city=" + citys + "&key=" + key); // 访问指定URl获取天气信息
   int httpCode1 = http.GET();                         // 接受HTTP相应状态码
-  String response = http.getString();                 // 获取天气正文
+  response = http.getString();                        // 获取天气正文
   http.end();                                         // 结束HTTP连接
 
-  http.begin(url2);                    // 访问指定URl获取时间信息（感谢阿b的API）
-  int httpCode2 = http.GET();          // 接受HTTP相应状态码
-  String response2 = http.getString(); // 获取时间正文
-  http.end();                          // 结束HTTP连接
+  http.begin(url2);             // 访问指定URl获取时间信息（感谢阿b的API）
+  int httpCode2 = http.GET();   // 接受HTTP相应状态码
+  response2 = http.getString(); // 获取时间正文
+  http.end();                   // 结束HTTP连接
 
   u8g2.setCursor(0, 60); // 显示获取状态
   if (httpCode1 == 200 && httpCode2 == 200)
     u8g2.printf("时间与天气获取成功");
   else
-    u8g2.printf("HTTP获取失败");
+  {
+    u8g2.printf("时间与天气获取失败");
+    HTTP_LinkError_Handle(); // 联网失败处理函数
+  }
   u8g2.sendBuffer();
 
   // 解析并转换JSON天气数据
@@ -134,124 +140,125 @@ void setup()
 void loop()
 {
   // OLED帧起始
-  u8g2.clearBuffer();
-  // 部分一：按键检测，切换模式
-  // if (digitalRead(17) == 0)
-  //{
-  //  delay(20);
-  //  if (digitalRead(17) == 0 && key_flag == 0)
-  //  {
-  //    if (key_num == 1)
-  //      key_num = 0;
-  //    else if (key_num == 0)
-  //      key_num = 1;
-  //    key_flag = 1;
-  //  }
-  //}
-  // else if (digitalRead(17) != 0)
-  //  key_flag = 0;
 
   weather_update(); // 天气更新
+  Button_Scan();    // 按键扫描
 
-  // 模式1，显示时间
-  if (mode_flag == 0)
-  { /*===========================================*/
-    // 模式一，正常时间显示界面
-    /*===========================================*/
-    u8g2.setFont(u8g2_font_logisoso32_tn);
-    u8g2.setCursor(0, 32);
-    // 小时
-    if (rtc.getHour() < 10)
-      u8g2.printf("0%d", rtc.getHour());
-    else
-      u8g2.printf("%d", rtc.getHour());
-    // 分隔
-    u8g2.printf(":");
-    // 分钟
-    if (rtc.getMinute() < 10)
-      u8g2.printf("0%d", rtc.getMinute());
-    else
-      u8g2.printf("%d", rtc.getMinute());
-    // 秒
-    u8g2.setFont(u8g2_font_logisoso16_tn);
-    u8g2.setCursor(100, 16);
-
-    if (rtc.getSecond() < 10)
-      u8g2.printf("0%d", rtc.getSecond());
-    else
-      u8g2.printf("%d", rtc.getSecond());
-    // AM/PM
-    u8g2.setFont(u8g2_font_profont22_tr);
-    u8g2.setCursor(100, 32);
-    String nowtime = rtc.getAmPm();
-    char *c = (char *)nowtime.c_str();
-    u8g2.printf("%s", c);
-    u8g2.drawHLine(0, 36, 128);
-    u8g2.setFont(u8g2_font_wqy12_t_gb2312);
-    u8g2.setCursor(0, 48);
-    u8g2.printf("天气： %s %d℃", weather, temperature);
-    u8g2.setCursor(0, 62);
-    u8g2.printf("风力： %s %s级", winddirection, windpower);
-    // 显示天气图标
-    u8g2.setCursor(110, 48);
-    u8g2.setFontMode(0);
-    u8g2.setFont(u8g2_font_open_iconic_weather_2x_t);
-    if (weather == "晴" && rtc.getHour(true) > 6 && rtc.getHour(true) <= 17) // 上午7点到下午5点为白天
-      u8g2.drawGlyph(105, 58, 0x45);
-    else if (weather == "晴" && rtc.getHour(true) > 17 && rtc.getHour(true) <= 6) // 下午6点到第二天6点为晚上
-      u8g2.drawGlyph(105, 58, 0x42);
-    else if (weather == "阴")
-      u8g2.drawGlyph(105, 58, 0x41);
-    else if (weather == "多云")
-      u8g2.drawGlyph(105, 58, 0x40);
-    else if (weather == "小雨" || weather == "中雨" || weather == "大雨" || weather == "暴雨" || weather == "雨" || weather == "阵雨")
-      u8g2.drawGlyph(105, 58, 0x43);
-    else
-      u8g2.drawGlyph(105, 58, 0x40);
-  }
-
-  // 模式2，显示天气
-  else if (mode_flag == 1)
+  if (Time_count % 100 == 0) // 1s刷新一次
   {
-    /*===========================================*/
-    // 模式二，详细天气显示界面
-    /*===========================================*/
-    u8g2.setFont(u8g2_font_wqy12_t_gb2312);
-    u8g2.setCursor(0, 12);
-    u8g2.printf("天气： %s %d℃", weather, temperature);
-    u8g2.setCursor(0, 24);
-    u8g2.printf("风力： %s %s级", winddirection, windpower);
-    // 显示天气图标
-    u8g2.setCursor(110, 25);
-    u8g2.setFontMode(0);
-    u8g2.setFont(u8g2_font_open_iconic_weather_2x_t);
-    if (weather == "晴" && rtc.getHour(true) > 6 && rtc.getHour(true) <= 17) // 上午7点到下午5点为白天
-      u8g2.drawGlyph(105, 25, 0x45);
-    else if (weather == "晴" && rtc.getHour(true) > 17 && rtc.getHour(true) <= 6) // 下午6点到第二天6点为晚上
-      u8g2.drawGlyph(105, 25, 0x42);
-    else if (weather == "阴")
-      u8g2.drawGlyph(105, 25, 0x41);
-    else if (weather == "多云")
-      u8g2.drawGlyph(105, 25, 0x40);
-    else if (weather == "小雨" || weather == "中雨" || weather == "大雨" || weather == "暴雨" || weather == "雨" || weather == "阵雨")
-      u8g2.drawGlyph(105, 25, 0x43);
-    else
-      u8g2.drawGlyph(105, 25, 0x40);
-    u8g2.setFont(u8g2_font_wqy12_t_gb2312);
-    u8g2.setCursor(0, 36);
-    float PotValue = analogRead(34) * 0.001705;
-    u8g2.printf("电池电量：%.1fV", PotValue);
-    u8g2.setCursor(0, 50);
-    u8g2.printf("天气更新时间：");
-    u8g2.setFont(u8g2_font_6x10_mf);
-    u8g2.setCursor(0, 64);
-    u8g2.printf("%s", reporttime.c_str());
+    u8g2.clearBuffer();
+    if (mode_flag == 0) // 模式1，显示时间
+    {
+      Display_Mode1();
+    }
+    else if (mode_flag == 1) // 模式2，显示天气
+    {
+      Display_Mode2();
+    }
+    Time_count = 0; // 作为循环中时间最长的进程，执行完之后就清除计数值
   }
 
-  // 帧结尾，500ms刷新一次
-  u8g2.sendBuffer();
-  delay(500);
+  // 计数装置
+  Time_count++;
+  delay(10);
 }
+
+void Display_Mode1(void)
+{
+  /*===========================================*/
+  // 模式一，正常时间显示界面
+  /*===========================================*/
+  u8g2.setFont(u8g2_font_logisoso32_tn);
+  u8g2.setCursor(0, 32);
+  // 小时
+  if (rtc.getHour() < 10)
+    u8g2.printf("0%d", rtc.getHour());
+  else
+    u8g2.printf("%d", rtc.getHour());
+  // 分隔
+  u8g2.printf(":");
+  // 分钟
+  if (rtc.getMinute() < 10)
+    u8g2.printf("0%d", rtc.getMinute());
+  else
+    u8g2.printf("%d", rtc.getMinute());
+  // 秒
+  u8g2.setFont(u8g2_font_logisoso16_tn);
+  u8g2.setCursor(100, 16);
+
+  if (rtc.getSecond() < 10)
+    u8g2.printf("0%d", rtc.getSecond());
+  else
+    u8g2.printf("%d", rtc.getSecond());
+  // AM/PM
+  u8g2.setFont(u8g2_font_profont22_tr);
+  u8g2.setCursor(100, 32);
+  String nowtime = rtc.getAmPm();
+  char *c = (char *)nowtime.c_str();
+  u8g2.printf("%s", c);
+  u8g2.drawHLine(0, 36, 128);
+  u8g2.setFont(u8g2_font_wqy12_t_gb2312);
+  u8g2.setCursor(0, 48);
+  u8g2.printf("天气： %s %d℃", weather, temperature);
+  u8g2.setCursor(0, 62);
+  u8g2.printf("风力： %s %s级", winddirection, windpower);
+  // 显示天气图标
+  u8g2.setCursor(110, 48);
+  u8g2.setFontMode(0);
+  u8g2.setFont(u8g2_font_open_iconic_weather_2x_t);
+  if (weather == "晴" && rtc.getHour(true) > 6 && rtc.getHour(true) <= 17) // 上午7点到下午5点为白天
+    u8g2.drawGlyph(105, 58, 0x45);
+  else if (weather == "晴" && rtc.getHour(true) > 17 && rtc.getHour(true) <= 6) // 下午6点到第二天6点为晚上
+    u8g2.drawGlyph(105, 58, 0x42);
+  else if (weather == "阴")
+    u8g2.drawGlyph(105, 58, 0x41);
+  else if (weather == "多云")
+    u8g2.drawGlyph(105, 58, 0x40);
+  else if (weather == "小雨" || weather == "中雨" || weather == "大雨" || weather == "暴雨" || weather == "雨" || weather == "阵雨")
+    u8g2.drawGlyph(105, 58, 0x43);
+  else
+    u8g2.drawGlyph(105, 58, 0x40);
+  u8g2.sendBuffer();
+}
+
+void Display_Mode2(void)
+{
+  /*===========================================*/
+  // 模式二，详细天气显示界面
+  /*===========================================*/
+  u8g2.setFont(u8g2_font_wqy12_t_gb2312);
+  u8g2.setCursor(0, 12);
+  u8g2.printf("天气： %s %d℃", weather, temperature);
+  u8g2.setCursor(0, 24);
+  u8g2.printf("风力： %s %s级", winddirection, windpower);
+  // 显示天气图标
+  u8g2.setCursor(110, 25);
+  u8g2.setFontMode(0);
+  u8g2.setFont(u8g2_font_open_iconic_weather_2x_t);
+  if (weather == "晴" && rtc.getHour(true) > 6 && rtc.getHour(true) <= 17) // 上午7点到下午5点为白天
+    u8g2.drawGlyph(105, 25, 0x45);
+  else if (weather == "晴" && rtc.getHour(true) > 17 && rtc.getHour(true) <= 6) // 下午6点到第二天6点为晚上
+    u8g2.drawGlyph(105, 25, 0x42);
+  else if (weather == "阴")
+    u8g2.drawGlyph(105, 25, 0x41);
+  else if (weather == "多云")
+    u8g2.drawGlyph(105, 25, 0x40);
+  else if (weather == "小雨" || weather == "中雨" || weather == "大雨" || weather == "暴雨" || weather == "雨" || weather == "阵雨")
+    u8g2.drawGlyph(105, 25, 0x43);
+  else
+    u8g2.drawGlyph(105, 25, 0x40);
+  u8g2.setFont(u8g2_font_wqy12_t_gb2312);
+  u8g2.setCursor(0, 36);
+  float PotValue = analogRead(34) * 0.001705;
+  u8g2.printf("电池电量：%.1fV", PotValue);
+  u8g2.setCursor(0, 50);
+  u8g2.printf("天气更新时间：");
+  u8g2.setFont(u8g2_font_6x10_mf);
+  u8g2.setCursor(0, 64);
+  u8g2.printf("%s", reporttime.c_str());
+  u8g2.sendBuffer();
+}
+
 /**
  * @brief 联网以更新天气数据
  */
@@ -305,17 +312,65 @@ void weather_update(void)
     Update_Flag = 0; // 避免反复触发
 }
 
-void Button_CallBack(void)
+void Button_Scan(void)
 {
-  // 古法延时
-  for (uint16_t i = 0; i < 65534; i++)
-    ;
-
-  if (digitalRead(17) == LOW)
+  if (digitalRead(17) == LOW && Button_Flag == 0)
   {
-    if (mode_flag == 1)
-      mode_flag = 0;
-    else if (mode_flag == 0)
-      mode_flag = 1;
+    delay(20);
+    if (digitalRead(17 == LOW) && Button_Flag == 0)
+    {
+      u8g2.clearBuffer();
+      if (mode_flag == 1)
+      {
+        mode_flag = 0;
+        Display_Mode1();
+      }
+      else if (mode_flag == 0)
+      {
+        mode_flag = 1;
+        Display_Mode2();
+      }
+      Button_Flag = 1; // 标志等于1表示被按下
+    }
   }
+  else if (digitalRead(17) == HIGH && Button_Flag == 1)
+  {
+    Button_Flag = 0; // 标志等于0表示未被按下
+  }
+}
+
+void HTTP_LinkError_Handle(void)
+{
+  int Link_Time = 0; // 记录重连次数
+  int httpCode1 = 0;
+  int httpCode2 = 0;
+  do
+  {
+    // 重新获取连接
+    HTTPClient http;                                    // 创建HTTP对象
+    http.begin(url + "?city=" + citys + "&key=" + key); // 访问指定URl获取天气信息
+    httpCode1 = http.GET();                             // 接受HTTP相应状态码
+    response = http.getString();                        // 获取天气正文
+    http.end();                                         // 结束HTTP连接
+
+    http.begin(url2);             // 访问指定URl获取时间信息
+    httpCode2 = http.GET();       // 接受HTTP相应状态码
+    response2 = http.getString(); // 获取时间正文
+    http.end();                   // 结束HTTP连接
+    Link_Time++;                  // 记录重连次数
+    // 重新连接获取UI
+    u8g2.clearBuffer();
+    u8g2.setCursor(0, 12);
+    u8g2.printf("重新获取数据中(%d)", Link_Time);
+    u8g2.setCursor(0, 24);
+    u8g2.printf("HTTPCode1=%d", httpCode1);
+    u8g2.setCursor(0, 36);
+    u8g2.printf("HTTPCode2=%d", httpCode2);
+    u8g2.sendBuffer();
+    delay(100);
+  } while (httpCode1 != 200 && httpCode2 != 200);
+  u8g2.setCursor(0, 48);
+  u8g2.printf("重新获取成功！");
+  u8g2.sendBuffer();
+  delay(1000);
 }
